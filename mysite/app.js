6d9774e1de67b9b17bbd5f86ca4113f80027d1af
parkash067 <pk_bscs@yahoo.com>
2016-09-05 02:53:31 -0700

    site convert into nodejs

---
+++ mysite/app.js
@@ -0,0 +1,24 @@
+/// <reference path="./typings/node/node.d.ts" />
+/// <reference path="./typings/express/express.d.ts" />
+/// <reference path="./typings/body-parser/body-parser.d.ts" />
+"use strict";
+var express = require('express');
+var app = express();
+var path = require('path');
+//set configuration
+app.set('trust proxy', true);
+app.set('case sensitive routing', true);
+app.set('strict routing', true);
+//set view-engine
+app.set('views', path.join(__dirname, './views/'));
+app.set('view engine', 'ejs');
+//set middleware
+app.use(express.static(path.join(__dirname, './public')));
+app.get('/', function (req, res) {
+    res.render("index");
+});
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+    var listenport = server.address().port;
+    console.log("Server is listening at port" + port);
+});
